255e80e8e249219ba8e73dc0893b237d0803c150
339713273 <339713273@2260I-L29-026>
2024-10-10 09:37:27 -0400

    Add project files.

---
+++ Loop pratice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata;
+
+namespace Loop_pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user;
+            Console.WriteLine("Hello user, Welcome to Todd's Casino, you will start off with 3 points and for every game of heads and tails you win, you will gain one point, and every loss you will lose 1 point, you can walk away at anytime! Have fun! ");
+            Console.WriteLine("Are you ready to play?");
+            user = Console.ReadLine().ToLower();
+            if (user == "Yes") ;
+            {
+                Console.WriteLine("Ok, heads or tails?");
+            }
+            
+        }
+    }
+}
